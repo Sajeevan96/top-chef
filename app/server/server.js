429874c9da83319836df0ca9aaf2d4b45e332429
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-02-07 23:36:21 +0100

    michelin module done

--- app/server/server.js
+++ app/server/server.js
@@ -1,34 +1,80 @@
-var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app = express();
 
-app.get('/scrape', function(req,res){
-    //URL that we want to scrape
-    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+function get_number_pages(url, callback){	
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			var number = $('.mr-pager-item').eq(-4).text();
+			callback(number);
+		}
+	});
+}
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-            var name;
-            var json = { name : ""};             
-            
-           $('.poi_card-display-title').each(function(){
-                var data = $(this);
-                console.log(data.text());
-                name = data.text();
-                json.name = name;
+function get_urls_in_resultpage(url, callback){
+	var urls_array = [];
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			$('a[class=poi-card-link]').each(function (i, element) {
+                urls_array.push('https://restaurant.michelin.fr' + $(element).attr('href'));
+            });
+            callback(urls_array);
+		}
+	});
+}
 
-                fs.writeFile('output.json', JSON.stringify(json,null, 2),function(err){
-                    console.log('File successfully written ! - Check output.json file');
-                });
-            })
-        }
-        res.send('Check your console!');
-    });
-});
+function get_page(url, callback){
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			var name = $('.poi_intro-display-title').first().text();
+			var adresse = $('.thoroughfare').first().text();
+			var code_postal = $('.postal-code').first().text();
+			var ville = $('.locality').first().text(); 
+			var description = $('.node_poi-cooking-types').first().text();
+			var chef = $('.field--name-field-chef').children('.field__items').first().text();
 
-app.listen('8081');
-console.log('Port 8081 ON');
-exports = module.exports = app;
+			var restaurant = {
+                "name": name,
+                "address": adresse,
+                "zipcode": code_postal,
+                "city": ville,
+                "description": description,
+                "chef": chef, 
+                "url": url
+            };
+            callback(restaurant);
+		}
+	});
+}
+
+function scraping(url){
+	var json = { "starred_restaurants": [] };
+	get_number_pages(url, function(number){
+		var i = 1; //Number of the page
+		var counter = 0;
+		while(i <= number){
+			get_urls_in_resultpage(url + '/page' + i.toString(), function(urls_array){
+				urls_array.forEach(function(element){
+					get_page(element, function(restaurant){
+						json.starred_restaurants.push(restaurant);
+						fs.writeFile('output.json', JSON.stringify(json), 'utf8', function(error){
+							if(error) {
+								return console.log(error);
+							} else{
+								counter++;
+								console.log("Restaurant " + counter + " added");
+							}
+						})
+					});
+				});
+			});	
+			i++;
+		}
+	});	
+}
+
+scraping('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin');
+console.log('Port 8080 ON');
